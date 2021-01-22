2ffebe0af19105d1e45c2e256bdce6a63c2e7661
karolbis <61646274+karolbis@users.noreply.github.com>
2021-01-22 19:25:06 +0100

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,23 @@
+//Zadanie 1
+const num = 1,
+      str = 'xD',
+      combo = num + str,
+      bool = true,
+      nothing = null;
+
+  console.log(num);
+  console.log(str);
+  console.log(combo);
+  console.log(bool);
+  console.log(nothing);
+
+//Zadanie 2
+let num1 = 5,
+      num2 = 6,
+      sum = 0;
+sum = num1 + num2;
+console.log(sum);
+
+//Zadanie 3
+let int;
+console.log(int);
