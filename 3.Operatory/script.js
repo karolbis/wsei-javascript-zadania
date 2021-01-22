1f8ae8c0eddb20ec635cf4d263f40ee523d3a1a4
karolbis <61646274+karolbis@users.noreply.github.com>
2021-01-22 19:25:41 +0100

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,48 @@
+//Zadanie 1 
+let x = true,
+    y = false;
+
+console.log(x === y);
+console.log(x != y);
+
+//Zadanie 2
+const num1 = 10;
+const num2 = 3;
+
+let moduloResult = 0;
+moduloResult = num1 / num2;
+console.log(moduloResult);
+
+//Zadanie 3
+let stringResult = "",
+    a = "test ",
+    b = "foo";
+stringResult = a + b;
+console.log(stringResult);
+
+//Zadanie 4
+var someNumber = 425;
+var someString = "425";
+
+console.log(someNumber == someString);
+console.log(someNumber === someString);
+
+//Zadanie 5
+
+let counter = 30;
+console.log(counter);
+
+counter++;
+console.log(counter);
+
+counter--;
+console.log(counter);
+
+//Zadanie 6
+
+let num5 = 15,
+    num6 = 6,
+result = null;
+
+result = num5 > num6;
+console.log(result);
